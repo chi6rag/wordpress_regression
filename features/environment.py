c8b3e0beffe3f877b99d22f6a4bbe79f86a95c7d
Chirag Aggarwal <me@chi6rag.net>
2016-03-02 10:00:12 +0530

    Setting up desired capabilities and configuration for tests

---
+++ features/environment.py
@@ -0,0 +1,18 @@
+from mhlib import PATH
+
+from appium import webdriver
+
+
+def before_all(context):
+    desired_capabilities = {}
+    desired_capabilities['platformName'] = 'Android'
+    desired_capabilities['automationName'] = 'Appium'
+    desired_capabilities['platformVersion'] = '5.1'
+    desired_capabilities['deviceName'] = 'Nexus 5'
+    desired_capabilities['app'] = '/Users/chi6rag/workspace/talks/pydelhi_05032016/spikes/wordpress_spike/apks/wordpress_android.apk'
+    context.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_capabilities)
+
+
+def after_all(context):
+    print("Executing after all")
+    context.driver.close_app
