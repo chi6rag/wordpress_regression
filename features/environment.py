ac83fb647d0115b6cf574d7a3416a477eb16f459
Chirag Aggarwal <me@chi6rag.net>
2016-03-03 10:26:59 +0530

    Dynamically picking app path

--- features/environment.py
+++ features/environment.py
@@ -1,13 +1,15 @@
+import os
 from appium import webdriver
 
 
 def before_all(context):
+    app_path = os.path.abspath('../wordpress_regression/apks/wordpress_android.apk')
     desired_capabilities = {}
     desired_capabilities['platformName'] = 'Android'
     desired_capabilities['automationName'] = 'Appium'
     desired_capabilities['platformVersion'] = '5.1'
     desired_capabilities['deviceName'] = 'Nexus 5'
-    desired_capabilities['app'] = '/Users/chi6rag/workspace/talks/pydelhi_05032016/spikes/wordpress_regression/apks/wordpress_android.apk'
+    desired_capabilities['app'] = app_path
     context.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_capabilities)
 
 
